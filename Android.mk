914853562e01f22d252c5753a124dbedfa5b4fea
Sam Lantinga <slouken@libsdl.org>
2011-01-13 19:04:57 -0800

    Working mikmod for Android!  Woo! :)

--- Android.mk
+++ Android.mk
@@ -4,12 +4,16 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := SDL_mixer
 
-LOCAL_CFLAGS := -I$(LOCAL_PATH)/.. -I$(LOCAL_PATH)/../SDL/include \
-	-DWAV_MUSIC -DOGG_MUSIC -DOGG_USE_TREMOR #-DMOD_MUSIC
+LOCAL_C_INCLUDES := \
+	$(LOCAL_PATH)/.. \
+	$(LOCAL_PATH)/../SDL/include \
+	$(LOCAL_PATH)/../mikmod/include \
+
+LOCAL_CFLAGS := -DWAV_MUSIC -DOGG_MUSIC -DOGG_USE_TREMOR -DMOD_MUSIC
 
 LOCAL_SRC_FILES := $(notdir $(filter-out %/playmus.c %/playwave.c, $(wildcard $(LOCAL_PATH)/*.c)))
 
-LOCAL_SHARED_LIBRARIES := SDL
+LOCAL_SHARED_LIBRARIES := SDL mikmod
 LOCAL_STATIC_LIBRARIES := tremor
 
 include $(BUILD_SHARED_LIBRARY)
