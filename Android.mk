e6a1760185f85f120bed7fb2198ea06917d7e479
Sam Lantinga <slouken@libsdl.org>
2012-01-30 20:09:58 -0500

    Updated Android Makefile for SDL2

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := SDL_mixer
+LOCAL_MODULE := SDL2_mixer
 
 LOCAL_C_INCLUDES := \
 	$(LOCAL_PATH)/.. \
@@ -13,7 +13,7 @@ LOCAL_CFLAGS := -DWAV_MUSIC -DOGG_MUSIC -DOGG_USE_TREMOR -DMOD_MUSIC
 
 LOCAL_SRC_FILES := $(notdir $(filter-out %/playmus.c %/playwave.c, $(wildcard $(LOCAL_PATH)/*.c)))
 
-LOCAL_SHARED_LIBRARIES := SDL mikmod
+LOCAL_SHARED_LIBRARIES := SDL2 mikmod
 LOCAL_STATIC_LIBRARIES := tremor
 
 include $(BUILD_SHARED_LIBRARY)
