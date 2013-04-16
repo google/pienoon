1cad2cc0113e1c05da4adb7427c732cdf25def4d
Grazvydas Ignotas <notasas@gmail.com>
2013-04-16 19:38:03 +0300

    Fix ogg seek when tremor is used
    ov_time_seek takes milliseconds, not seconds.

--- music_ogg.c
+++ music_ogg.c
@@ -225,7 +225,7 @@ void OGG_delete(OGG_music *music)
 void OGG_jump_to_time(OGG_music *music, double time)
 {
 #ifdef OGG_USE_TREMOR
-       vorbis.ov_time_seek( &music->vf, (ogg_int64_t)time );
+       vorbis.ov_time_seek( &music->vf, (ogg_int64_t)(time * 1000.0) );
 #else
        vorbis.ov_time_seek( &music->vf, time );
 #endif
