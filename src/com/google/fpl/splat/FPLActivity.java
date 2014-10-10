f27bd163421df7b3a788e7aa40bb26a92e0651ce
Chris Cornell <ccornell@google.com>
2014-10-10 13:57:05 -0700

    Enabled fullscreen immersive mode.
    The android nav buttons (home, back, etc)  now automatically hide
    themselves after the game starts, and require an inward-swipe to
    make visible.
    
    Tested on Nexus 5, Molly
    
    Change-Id: Ifbc96c4590c286be9aa95f26cf25637ad42c6109

--- src/com/google/fpl/splat/FPLActivity.java
+++ src/com/google/fpl/splat/FPLActivity.java
@@ -6,6 +6,7 @@ package com.google.fpl.splat;
 import org.libsdl.app.SDLActivity;
 import android.content.Intent;
 import android.app.Activity;
+import android.view.View;
 
 public class FPLActivity extends SDLActivity {
     // GPG's GUIs need activity lifecycle events to function properly, but
@@ -15,6 +16,21 @@ public class FPLActivity extends SDLActivity {
       super.onActivityResult(requestCode, resultCode, data);
       nativeOnActivityResult(this, requestCode, resultCode, data);
     }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+      super.onWindowFocusChanged(hasFocus);
+      if (hasFocus) {
+        mLayout.setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+      }
+    }
+
     // Implemented in C++.
     private static native void nativeOnActivityResult(
         Activity activity,
