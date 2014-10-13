4eb30767d85c2888f7d4d2efb9b207715df44a7e
Alex Ames <amablue@google.com>
2014-10-13 14:33:36 -0700

    bulid_assets.py now checks for a prebuild cwep executable.
    Now that we're shipping a prebuilt cwebp executable in the prebuilts
    directory, use that unless a version of cwebp has been built from source
    
    Change-Id: I77b34c165640081403be13d6160d74bbf110454b

--- scripts/build_assets.py
+++ scripts/build_assets.py
@@ -34,6 +34,9 @@ import sys
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__),
                                             os.path.pardir))
 
+PREBUILTS_ROOT = os.path.abspath(os.path.join(os.path.join(PROJECT_ROOT),
+    os.path.pardir, os.path.pardir, os.path.pardir, os.path.pardir))
+
 # Directories that may contains the FlatBuffers compiler.
 FLATBUFFERS_PATHS = [
     os.path.join(PROJECT_ROOT, 'bin'),
@@ -46,6 +49,7 @@ CWEBP_PATHS = [
     os.path.join(PROJECT_ROOT, 'bin'),
     os.path.join(PROJECT_ROOT, 'bin', 'Release'),
     os.path.join(PROJECT_ROOT, 'bin', 'Debug'),
+    os.path.join(PREBUILTS_ROOT, 'libwebp', 'libwebp-0.4.1-linux-x86-32', 'bin')
 ]
 
 # Directory to place processed assets.
