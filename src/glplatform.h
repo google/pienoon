7798521543ef1c614ecd91a43e2ab3402b1da62c
Johan Euphrosine <proppy@google.com>
2015-03-16 15:27:26 -0700

    glplatform: fix ios define

--- src/glplatform.h
+++ src/glplatform.h
@@ -19,7 +19,7 @@
 
 #ifdef __APPLE__
     #include "TargetConditionals.h"
-    #ifdef __IOS__
+    #ifdef TARGET_OS_IPHONE
         #define PLATFORM_MOBILE
         //#include <SDL_opengles2.h>
         #include <OpenGLES/ES2/gl.h>
